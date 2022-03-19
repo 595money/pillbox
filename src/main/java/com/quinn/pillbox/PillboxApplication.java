f1f564fa1ae4ad17804e0e11b22f2a7a0a4888f4
ubuntu_quinn <595money@gmail.com>
2022-03-19 18:52:08 +0800

    chore: gradle clean build 時自動加載失敗
    cause by: 專案此時尚未有資料庫連線設定, 因此在自動加載時引發此錯誤,
    modify: 於 PillboxApplication.java  增加  @SpringBootApplication  排除 DataSourceAutoConfiguration.clas

--- src/main/java/com/quinn/pillbox/PillboxApplication.java
+++ src/main/java/com/quinn/pillbox/PillboxApplication.java
@@ -2,8 +2,10 @@ package com.quinn.pillbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// exclude DataSourceAutoConfiguration.class, 在資料庫連線設定完成前都須排除
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class PillboxApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/com/quinn/pillbox/QuinnTest.java
@@ -0,0 +1,32 @@
+/**   
+* @Title: QuinnTest.java 
+* @Package com.quinn.pillbox 
+* @Description: TODO 
+* @author pigmilk
+* @date Mar 19, 2022 7:29:34 AM 
+* @version 1.0.0   
+*/
+package com.quinn.pillbox;
+
+/** 
+* @ClassName: QuinnTest 
+* @Description: 東測西測
+* @author pigmilk
+* @date Mar 19, 2022 7:29:34 AM  
+*/
+public class QuinnTest {
+
+	/** 
+	* @Title: main 
+	* @Description: 寧波的號碼是空號
+	* @param @param args  引數說明 
+	* @return void 返回型別 
+	* @throws 
+	*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola jdk 17");
+
+	}
+
+}
