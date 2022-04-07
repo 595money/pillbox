0a25fa0b3e7bca348473fd240dc35a970afb3b1d
ubuntu_quinn <595money@gmail.com>
2022-04-08 05:55:47 +0800

    chore: 增加 dev_backend_quinn 的 remote tracking

--- src/main/java/com/quinn/pillbox/PillboxApplication.java
+++ src/main/java/com/quinn/pillbox/PillboxApplication.java
@@ -5,11 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 // exclude DataSourceAutoConfiguration.class, 在資料庫連線設定完成前都須排除
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class PillboxApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PillboxApplication.class, args);
 	}
-
 }
