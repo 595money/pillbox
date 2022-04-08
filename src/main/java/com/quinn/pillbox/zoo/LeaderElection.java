d31e89c6aaf62549e915a838a98ef1f3ef1e9fb3
ubuntu_quinn <595money@gmail.com>
2022-04-08 17:44:28 +0800

    chore: 增加 web.xml

--- src/main/java/com/quinn/pillbox/zoo/LeaderElection.java
+++ src/main/java/com/quinn/pillbox/zoo/LeaderElection.java
@@ -7,22 +7,50 @@
 * @version 1.0.0   
 */
 package com.quinn.pillbox.zoo;
+
+import java.io.IOException;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
+
 /**
  * @ClassName: LeaderElection
  * @Description: TODO(這裡用一句話描述這個類的作用)
  * @author pigmilk
  * @date Apr 8, 2022 6:18:30 AM
  */
-public class LeaderElection {
-	
-	private static final String ZOOKEEPER_ADDRESS = "http://172.17.0.4:2181";
-	private ZooKeeper Zookeeper;
-	public void connectToZookeeper() {
+public class LeaderElection implements Watcher{
 
+	private static final String ZOOKEEPER_ADDRESS = "172.17.0.4:2181";
+	private static final int SESSION_TIMEOUT = 3000;
+	private ZooKeeper zooKeeper;
+
+	public void connectToZookeeper() throws IOException {
+		this.zooKeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
 	}
 	
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
+		LeaderElection leaderElection = new LeaderElection();
+		leaderElection.connectToZookeeper();
 
+	}
+
+	/*
+	* Title: process
+	* Description: 
+	* @param event 
+	* @see org.apache.zookeeper.Watcher#process(org.apache.zookeeper.WatchedEvent) 
+	*/
+	@Override
+	public void process(WatchedEvent event) {
+		switch(event.getType()) {
+		case None:
+			if(event.getState() == Event.KeeperState.SyncConnected) {
+				System.out.println("Sucessfully connected to Zookeeper");
+			}
+		}
+		
 	}
 }
